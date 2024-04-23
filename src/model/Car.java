ff739d23accb615fe59022e1c65b158879db56af
Stefani <stefanigtorres@gmail.com>
2024-04-22 21:39:48 -0500

    Feat: creat DriveShaft and Car

---
+++ src/model/Car.java
@@ -0,0 +1,16 @@
+package model;
+
+public class Car {
+
+    private DriveShaft driveShaft;
+    private Tire tire;
+
+    public Car(DriveShaft driveShaft, Tire tire) {
+        this.DriveShaft = driveShaft;
+        this.Tire = tire;
+    }
+
+    public Car() {
+    }
+
+}
