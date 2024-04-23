72751dd39c4d7a6388804327798ed3dd071d2e1c
Stefani <stefanigtorres@gmail.com>
2024-04-22 22:29:10 -0500

    Feat: modify Car class

--- src/model/Car.java
+++ src/model/Car.java
@@ -1,16 +1,50 @@
 package model;
 
+import java.util.Arrays;
+
 public class Car {
 
     private DriveShaft driveShaft;
-    private Tire tire;
+    private Tire[] tires;
 
-    public Car(DriveShaft driveShaft, Tire tire) {
-        this.DriveShaft = driveShaft;
-        this.Tire = tire;
+    public Car(DriveShaft driveShaft) {
+        this.driveShaft = driveShaft;
+        this.tires = new Tire[4];
     }
 
     public Car() {
     }
 
+    public String createTire(Tire tire) {
+        String message = "";
+        for (int i = 0; i < tires.length ; i++) {
+            if (quantityTire() < 4) {
+                if (tires[i] == null){
+                    tires[i] = tire;
+                    return message = "Nueva llanta agregada";
+                }
+            }else {
+                return message = "No puede tener más de 4 llantas";
+            }
+        }
+        return message;
+    }
+
+    public int quantityTire() {
+        int count = 0;
+        for (Tire tire:tires) {
+            if (tire != null) {
+                count ++;
+            }
+        }
+        return count;
+    }
+
+    @Override
+    public String toString() {
+        return "Car{" +
+                "driveShaft=" + driveShaft +
+                ", tires=" + Arrays.toString(tires) +
+                '}';
+    }
 }
